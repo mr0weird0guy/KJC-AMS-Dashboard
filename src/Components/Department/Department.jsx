3ae9d1ade64ca2556a40a1c1096213a92dfa0cfe
Shakir <shakirali.sacube@gmail.com>
2023-06-28 19:30:47 +0530

    Modified sidebar according to the updated design

---
+++ src/Components/Department/Department.jsx
@@ -0,0 +1,17 @@
+const Department = () => {
+    const centerDiv = {
+      height: "100vh",
+      display: "flex",
+      justifyContent: "center",
+      alignItems: "center",
+    }
+  
+    return (
+      <div style={centerDiv}>
+        <h1>Department</h1>
+      </div>
+    )
+  }
+  
+  export default Department
+  
