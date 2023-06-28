2d24eed27da8205cbdd176f322bebb114eb51bfa
Rohan Agarwal <rohandiwan1824@gmail.com>
2023-06-28 23:58:25 +0530

    Header Update

--- src/Components/Events/Events.jsx
+++ src/Components/Events/Events.jsx
@@ -1,11 +1,14 @@
 import React,{useEffect} from "react";
 import "./Events.css";
+import {MdCalendarMonth} from "react-icons/md"
+import Header from "../Header/Header";
 import { Link, NavLink, Outlet } from 'react-router-dom'
 
 function Events() {
 
   return (
     <div className="events-main-container">
+      <Header icon={<MdCalendarMonth/>} text={"Events"}></Header>
       <div className="header-tabs-container">
         <div className="tabs-container">
           <NavLink to={"allevents"} className="nav-link">All Events</NavLink>
